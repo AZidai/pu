7087cf879045b22a1ab00fdc6424917c65e579ca
Aleksandar Zidai <aleksandar.zidai@gmail.com>
2019-03-11 15:56:45 +0100

    First commit

---
+++ resources/views/app.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>PU</title>
+        <link rel="stylesheet" href="dist/app.css" />
+    </head>
+    <body>
+        <div id="app">
+            <h1>PU</h1>
+
+        </div>
+        <script src="dist/app.js"></script>
+    </body>
+</html>
