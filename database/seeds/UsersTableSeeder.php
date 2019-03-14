18a237b49797f1f140b9d9179f30295a73fb89c8
Aleksandar Zidai <aleksandar.zidai@gmail.com>
2019-03-14 16:31:43 +0100

    [DB] Users migration and seeder

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+use Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeder extends Seeder
+{
+
+    public function run()
+    {
+        User::truncate();
+
+        $faker = \Faker\Factory::create();
+        $password = Hash::make('password');
+
+        User::create([
+            'name' => 'Administrator',
+            'email' => 'admin@test.com',
+            'password' => $password,
+        ]);
+
+        for ($i = 0; $i < 5; $i++) {
+            User::create([
+                'name' => $faker->name,
+                'email' => $faker->email,
+                'password' => $password,
+            ]);
+        }
+    }
+
+}
